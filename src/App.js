aa96af2f91b582a567aeebb5cad2397f0145646a
özcan çetin <cetin445@gmail.com>
2022-07-27 20:49:46 +0300

    title

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import data from "./data";
 import Question from "./Question";
 function App() {
   const [questions, setQuestions] = useState(data);
-  
+
   return <main>
-    
+    <div className="container">
+      <div className="title">
+      <h3>Questions and Answers About Login</h3>
+      </div>
+    </div>
   </main>;
 }
 
