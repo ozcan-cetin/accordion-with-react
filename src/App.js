603a6f384ed9631c5676a83fa70480efcc2d4a92
özcan çetin <cetin445@gmail.com>
2022-07-27 20:46:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./App.css"
+import data from "./data";
+import Question from "./Question";
+function App() {
+  const [questions, setQuestions] = useState(data);
+  
+  return <main>
+    
+  </main>;
+}
+
+export default App;
